fcc1f0319930f17a459175d2f33edfb56999e5d7
km1uk <keith@kgmm.co.uk>
2020-02-17 19:27:52 +0000

    1 file modified, 1 added, 1 deleted

---
+++ components/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <h1> Hello world</h1>
+        <ul>
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      </nav>
+      <main>
+        <p>Main content goes here...</p>
+      </main>
+    </div>
+  )
+}
+
+
+export default App
